7dc07c38b0f313a2703b531bf8dd2964025ac8a8
stecpiotr <stecpiotr82@gmail.com>
2025-06-24 23:52:29 +0200

    Questionnaire.tsx

--- ap48-ankieta/src/Questionnaire.tsx
+++ ap48-ankieta/src/Questionnaire.tsx
@@ -76,7 +76,7 @@ const Questionnaire: React.FC = () => {
           src="/BadaniaPRO(r).png"
           alt="Badania.pro logo"
           style={{
-            height: 60,
+            height: 45,
             width: "auto",
             marginLeft: 20,
             borderRadius: 7,
